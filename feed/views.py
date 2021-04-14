7cdc4c4bd51c22a3fea350579d86984cd0dc84be
Lisgevan <episimo@hotmail.com>
2021-04-14 07:01:12 +0300

    main

---
+++ feed/views.py
@@ -0,0 +1,15 @@
+#need this import in function based view only
+# from django.shortcuts import render
+
+#import for class based view
+from django.views.generic import TemplateView
+from .models import Post
+# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.all
+        return context
